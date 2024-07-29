9bf0d51ce7d05af1d476a0df3a80818f14bae477
ITACHI1097 <67718266+ITACHI1097@users.noreply.github.com>
2024-07-29 09:40:44 -0500

    Update script.sh

--- Image2/script.sh
+++ Image2/script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-/usr/local/bin/aws configure set aws_access_key_id AKIARUABTIHI76MZG7O7
-/usr/local/bin/aws configure set aws_secret_access_key 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
+/usr/local/bin/aws configure set aws_access_key_id X
+/usr/local/bin/aws configure set aws_secret_access_key X
 
 aws s3 cp /root/output/timestamp s3://test-terraform
